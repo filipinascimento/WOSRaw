574e8de67f13caa1c65fe638f3ea76c990d84816
Filipi Nascimento Silva <filipinascimento@gmail.com>
2024-12-25 06:48:10 +0000

    Updated paths

--- Examples/createRawArchive.py
+++ Examples/createRawArchive.py
@@ -4,19 +4,19 @@ from pathlib import Path
 
 
 #Path to the WoS zipped XML files 
-WOSPath = Path("/raw/WoS_2022/")
+WOSPath = Path("/mnt/sciencegenome/WOS_RAW/WoS_2023/")
 
 # Path where to save the WoS dbgz archive
-WOSArchivePath = Path("/gpfs/sciencegenome/WOS/WoS_2022_All.dbgz")
+WOSArchivePath = Path("/mnt/sciencegenome/WOS_DBGZ/WoS_2023_All.dbgz")
 
 # Path to the generated 
-WOSIndexPath = Path("/gpfs/sciencegenome/WOS/WoS_2022_All_byUID.idbgz")
+WOSIndexPath = Path("/mnt/sciencegenome/WOS_DBGZ/WoS_2023_All_byUID.idbgz")
+
+if __name__ == "__main__":
+    wos.archive.create(WOSPath, WOSArchivePath)
 
 
-wos.archive.create(WOSPath, WOSArchivePath)
-
-
-# This may take several hours to complete
-wos.archive.createIndexByUID(WOSArchivePath, WOSIndexPath)
+    # This may take several hours to complete
+    wos.archive.createIndexByUID(WOSArchivePath, WOSIndexPath)
 
 
